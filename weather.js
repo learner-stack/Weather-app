13f0883361483a5d875f88676de0aeaf5b16da22
AT071998 <67892006+AT071998@users.noreply.github.com>
2021-02-18 20:02:04 +0530

    Created a new app

---
+++ weather.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express()
+const bodyparser = require('body-parser');
+const request = require('request');
+app.set('view engine', 'ejs');
+app.engine('ejs', require('ejs').__express);
+const apiKey='6b3c03b781409edc8c4df3bcc1f60a5a';
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(express.static('public'));
+
+
+app.get('/',function(req,res){
+  res.render('index',{weather:null,error:null});
+});
+app.post('/',function(req,response){
+  let city = req.body.city;
+  //console.log(city);
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+  request(url,function(err,res,body){
+    if(!err){
+      let weather = JSON.parse(body);
+      if(weather.main == undefined){
+        response.render('index',{weather: null,error: 'Please enter a valid city Name'});
+      }else{
+        let displayweather = `Its ${weather.main.temp} degrees in ${weather.name}`;
+        response.render('index',{weather: displayweather,error: null});
+      }
+    }
+
+  });
+
+})
+
+app.listen(8080);
